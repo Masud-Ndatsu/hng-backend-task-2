5dbea4fd63cb5b5a1ed3380676c169f0b99063c1
Masud-Ndatsu <masudndastsu@gmail.com>
2022-10-27 21:29:38 +0100

    second commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors())
 
 app.get("/", (req, res) => {
     res.status(200).json({
-    "slackUsername": "mN",
+    "slackUsername": "biggysmall",
     "backend": true,
     "age": 23,
     "bio": "My name is Mas'ud Ndatsu and I have been coding for like 2 years now. I hope that with the connections and oppurtunities that comes with this internship. I can secure a backend (Nodejs) developer role. This is the sad truth about me :( ."
